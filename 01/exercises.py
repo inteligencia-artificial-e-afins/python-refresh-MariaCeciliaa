f607f9d6a6636937d31356d50368b1e6f4c78590
Laboratório Informática <labinfo@unifil.br>
2024-02-21 21:50:45 -0300

    desenvolvendo exercicios 1 e 2

--- 01/exercises.py
+++ 01/exercises.py
@@ -1,10 +1,18 @@
+# -*- coding: utf-8 -*-
+
 # Exercicio 01
 def max_consecutive_sum(nums):
-    # implementar a solução aqui
-    pass
+    current_sum = max_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
 
 # Testes 01
 def test_max_consecutive_sum():
+    print("Teste 1 - max_consecutive_sum")
     print(max_consecutive_sum([1, -3, 2, 1, -1]) == 3)
     print(max_consecutive_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
     print(max_consecutive_sum([5, -1, -2, 3, -1, 2, -4]) == 7)
@@ -12,13 +20,17 @@ def test_max_consecutive_sum():
     print(max_consecutive_sum([-1, -2, -3, -4, -5]) == -1)
 
 
+
 # Exercício 02
 def is_palindrome(word):
-    # implementar a solução aqui
-    pass
+    word = word.replace(" ", "").lower()
+
+    return word == word[::-1]
+
 
 # Testes 02
 def text_is_palindrome():
+    print("Teste 2 - is_palindrome")
     print(is_palindrome("radar") == True)
     print(is_palindrome("racecar") == True)
     print(is_palindrome("level") == True)
@@ -29,6 +41,7 @@ def text_is_palindrome():
     print(is_palindrome("abccbaa") == False)
 
 
+
 # Exercício 03
 def count_increasing_subsets(nums):
     # implementar a solução aqui
@@ -36,6 +49,7 @@ def count_increasing_subsets(nums):
 
 # Testes 03
 def test_count_increasing_subsets():
+    print("Teste 3 - count_increasing_subsets")
     # Teste com lista vazia
     print(count_increasing_subsets([]) == 0)
     # Teste com uma lista de um elemento
